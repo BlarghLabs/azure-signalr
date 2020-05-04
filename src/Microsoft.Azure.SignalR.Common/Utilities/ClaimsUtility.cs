797a8a6cb54b57a68bfb0c10efdd613f6c78b476
Brian Sobel <sobelito@gmail.com>
2020-05-04 03:54:19 -0400

    formatting

--- src/Microsoft.Azure.SignalR.Common/Utilities/ClaimsUtility.cs
+++ src/Microsoft.Azure.SignalR.Common/Utilities/ClaimsUtility.cs
@@ -7,146 +7,115 @@ using System.Linq;
 using System.Security.Claims;
 using Microsoft.Azure.SignalR.Protocol;
 
-namespace Microsoft.Azure.SignalR
-{
-    internal static class ClaimsUtility
+namespace Microsoft.Azure.SignalR {
+  internal static class ClaimsUtility {
+    private const string DefaultAuthenticationType = "Bearer";
+    private static readonly string[] SystemClaims =
     {
-        private const string DefaultAuthenticationType = "Bearer";
-        private static readonly string[] SystemClaims =
-        {
             "aud", // Audience claim, used by service to make sure token is matched with target resource.
             "exp", // Expiration time claims. A token is valid only before its expiration time.
             "iat", // Issued At claim. Added by default. It is not validated by service.
             "nbf"  // Not Before claim. Added by default. It is not validated by service.
         };
 
-        private static readonly ClaimsIdentity DefaultClaimsIdentity = new ClaimsIdentity();
-        private static readonly ClaimsPrincipal EmptyPrincipal = new ClaimsPrincipal(DefaultClaimsIdentity);
-        private static readonly string DefaultNameClaimType = DefaultClaimsIdentity.NameClaimType;
-        private static readonly string DefaultRoleClaimType = DefaultClaimsIdentity.RoleClaimType;
+    private static readonly ClaimsIdentity DefaultClaimsIdentity = new ClaimsIdentity();
+    private static readonly ClaimsPrincipal EmptyPrincipal = new ClaimsPrincipal(DefaultClaimsIdentity);
+    private static readonly string DefaultNameClaimType = DefaultClaimsIdentity.NameClaimType;
+    private static readonly string DefaultRoleClaimType = DefaultClaimsIdentity.RoleClaimType;
 
-        public static IEnumerable<Claim> BuildJwtClaims(ClaimsPrincipal user, string userId, Func<IEnumerable<Claim>> claimsProvider, string serverName = null, ServerStickyMode mode = ServerStickyMode.Disabled, bool enableDetailedErrors = false, int endpointsCount = 1)
-        {
-            if (userId != null)
-            {
-                yield return new Claim(Constants.ClaimType.UserId, userId);
-            }
+    public static IEnumerable<Claim> BuildJwtClaims(ClaimsPrincipal user, string userId, Func<IEnumerable<Claim>> claimsProvider, string serverName = null, ServerStickyMode mode = ServerStickyMode.Disabled, bool enableDetailedErrors = false, int endpointsCount = 1) {
+      if (userId != null) {
+        yield return new Claim(Constants.ClaimType.UserId, userId);
+      }
 
-            if (serverName != null && mode != ServerStickyMode.Disabled)
-            {
-                yield return new Claim(Constants.ClaimType.ServerName, serverName);
-                yield return new Claim(Constants.ClaimType.ServerStickyMode, mode.ToString());
-            }
+      if (serverName != null && mode != ServerStickyMode.Disabled) {
+        yield return new Claim(Constants.ClaimType.ServerName, serverName);
+        yield return new Claim(Constants.ClaimType.ServerStickyMode, mode.ToString());
+      }
 
-            var authenticationType = user?.Identity?.AuthenticationType;
+      var authenticationType = user?.Identity?.AuthenticationType;
 
-            // No need to pass it when the authentication type is Bearer
-            if (authenticationType != null && authenticationType != DefaultAuthenticationType)
-            {
-                yield return new Claim(Constants.ClaimType.AuthenticationType, authenticationType);
-            }
+      // No need to pass it when the authentication type is Bearer
+      if (authenticationType != null && authenticationType != DefaultAuthenticationType) {
+        yield return new Claim(Constants.ClaimType.AuthenticationType, authenticationType);
+      }
 
-            // Trace multiple instances
-            if (endpointsCount > 1)
-            {
-                yield return new Claim(Constants.ClaimType.ServiceEndpointsCount, endpointsCount.ToString());
-            }
+      // Trace multiple instances
+      if (endpointsCount > 1) {
+        yield return new Claim(Constants.ClaimType.ServiceEndpointsCount, endpointsCount.ToString());
+      }
 
-            if (enableDetailedErrors)
-            {
-                yield return new Claim(Constants.ClaimType.EnableDetailedErrors, true.ToString());
-            }
+      if (enableDetailedErrors) {
+        yield return new Claim(Constants.ClaimType.EnableDetailedErrors, true.ToString());
+      }
 
-            // Return custom NameClaimType and RoleClaimType
-            // We can have multiple Identities, for now, choose the default one 
-            if (user?.Identity is ClaimsIdentity identity)
-            {
-                var nameType = identity.NameClaimType;
-                if (nameType != null && nameType != DefaultNameClaimType)
-                {
-                    yield return new Claim(Constants.ClaimType.NameType, nameType);
-                }
-
-                var roleType = identity.RoleClaimType;
-                if (roleType != null && roleType != DefaultRoleClaimType)
-                {
-                    yield return new Claim(Constants.ClaimType.RoleType, roleType);
-                }
-            }
-
-            // return customer's claims
-            var customerClaims = claimsProvider == null ? user?.Claims : claimsProvider.Invoke();
-            if (customerClaims != null)
-            {
-                foreach (var claim in customerClaims)
-                {
-                    // Add AzureSignalRUserPrefix if customer's claim name is duplicated with SignalR system claims.
-                    // And split it when return from SignalR Service.
-                    if (SystemClaims.Contains(claim.Type))
-                    {
-                        yield return new Claim(Constants.ClaimType.AzureSignalRUserPrefix + claim.Type, claim.Value);
-                    }
-                    else
-                    {
-                        yield return claim;
-                    }
-                }
-            }
+      // Return custom NameClaimType and RoleClaimType
+      // We can have multiple Identities, for now, choose the default one 
+      if (user?.Identity is ClaimsIdentity identity) {
+        var nameType = identity.NameClaimType;
+        if (nameType != null && nameType != DefaultNameClaimType) {
+          yield return new Claim(Constants.ClaimType.NameType, nameType);
         }
 
-        public static ClaimsPrincipal GetUserPrincipal(this OpenConnectionMessage message)
-        {
-            if (message == null)
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
-
-            return GetUserPrincipal(message.Claims);
+        var roleType = identity.RoleClaimType;
+        if (roleType != null && roleType != DefaultRoleClaimType) {
+          yield return new Claim(Constants.ClaimType.RoleType, roleType);
         }
+      }
 
-        internal static ClaimsPrincipal GetUserPrincipal(Claim[] messageClaims)
-        {
-            if (messageClaims == null || messageClaims.Length == 0)
-            {
-                return EmptyPrincipal;
-            }
-
-            var claims = new List<Claim>();
-            var authenticationType = DefaultAuthenticationType;
-            string nameType = null;
-            string roleType = null;
-            foreach (var claim in messageClaims)
-            {
-                if (claim.Type == Constants.ClaimType.AuthenticationType)
-                {
-                    authenticationType = claim.Value;
-                }
-                else if (claim.Type == Constants.ClaimType.NameType)
-                {
-                    nameType = claim.Value;
-                }
-                else if (claim.Type == Constants.ClaimType.RoleType)
-                {
-                    roleType = claim.Value;
-                }
-                else if (claim.Type.StartsWith(Constants.ClaimType.AzureSignalRUserPrefix))
-                {
-                    var claimName = claim.Type.Substring(Constants.ClaimType.AzureSignalRUserPrefix.Length);
-                    claims.Add(new Claim(claimName, claim.Value));
-                }
-                else if (!SystemClaims.Contains(claim.Type) && !claim.Type.StartsWith(Constants.ClaimType.AzureSignalRSysPrefix))
-                {
-                    claims.Add(claim);
-                }
-            }
-
-            if (claims.Count == 0)
-            {
-                // For JWT token, the authenticated claims must contain non-system claims
-                return EmptyPrincipal;
-            }
-
-            return new ClaimsPrincipal(new ClaimsIdentity(claims, authenticationType, nameType, roleType));
+      // return customer's claims
+      var customerClaims = claimsProvider == null ? user?.Claims : claimsProvider.Invoke();
+      if (customerClaims != null) {
+        foreach (var claim in customerClaims) {
+          // Add AzureSignalRUserPrefix if customer's claim name is duplicated with SignalR system claims.
+          // And split it when return from SignalR Service.
+          if (SystemClaims.Contains(claim.Type)) {
+            yield return new Claim(Constants.ClaimType.AzureSignalRUserPrefix + claim.Type, claim.Value);
+          } else {
+            yield return claim;
+          }
         }
+      }
     }
+
+    public static ClaimsPrincipal GetUserPrincipal(this OpenConnectionMessage message) {
+      if (message == null) {
+        throw new ArgumentNullException(nameof(message));
+      }
+
+      return GetUserPrincipal(message.Claims);
+    }
+
+    internal static ClaimsPrincipal GetUserPrincipal(Claim[] messageClaims) {
+      if (messageClaims == null || messageClaims.Length == 0) {
+        return EmptyPrincipal;
+      }
+
+      var claims = new List<Claim>();
+      var authenticationType = DefaultAuthenticationType;
+      string nameType = null;
+      string roleType = null;
+      foreach (var claim in messageClaims) {
+        if (claim.Type == Constants.ClaimType.AuthenticationType) {
+          authenticationType = claim.Value;
+        } else if (claim.Type == Constants.ClaimType.NameType) {
+          nameType = claim.Value;
+        } else if (claim.Type == Constants.ClaimType.RoleType) {
+          roleType = claim.Value;
+        } else if (claim.Type.StartsWith(Constants.ClaimType.AzureSignalRUserPrefix)) {
+          var claimName = claim.Type.Substring(Constants.ClaimType.AzureSignalRUserPrefix.Length);
+          claims.Add(new Claim(claimName, claim.Value));
+        } else if (!SystemClaims.Contains(claim.Type) && !claim.Type.StartsWith(Constants.ClaimType.AzureSignalRSysPrefix)) {
+          claims.Add(claim);
+        }
+      }
+
+      if (claims.Count == 0) {
+        // For JWT token, the authenticated claims must contain non-system claims
+        return EmptyPrincipal;
+      }
+
+      return new ClaimsPrincipal(new ClaimsIdentity(claims, authenticationType, nameType, roleType));
+    }
+  }
 }
